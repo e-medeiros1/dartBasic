211dc2a352439a53c0ea4f0aa841f922600f828e
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-01-27 11:59:48 -0300

    Criando função genérica

---
+++ Funções/funcao_com_generics.dart
@@ -0,0 +1,14 @@
+//Uso do generic
+
+void main(List<String> args) {
+  var lista = [1, 2, 3, 4, 5, 6, 7];
+  print(retornaIndex(lista));
+}
+
+E retornaIndex<E>(List<E> lista) {
+  if (lista.length >= 2) {
+    return lista[1];
+  } else {
+    return null!;
+  }
+}
